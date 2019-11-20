16fb7cb4fa66f40ee206d3363816d10cfac465b2
Ildeberto de los Santos Ruiz <44104319+isantosruiz@users.noreply.github.com>
2019-11-20 16:35:22 +0100

    Add files via upload

--- direction.m
+++ direction.m
@@ -1,12 +1,17 @@
-function C = direction(X)
+function [C,M] = direction(X)
 %DIRECTION Direction cosines of the row-vectors in a matrix.
 %
 %   Syntax:
 %      C = DIRECTION(X)
 %
-%   Programmed by:
+%      X ... Matrix with row-vectors
+%      C ... Matrix with direction cosines
+%      M ... Column-vector with magnitudes
+%
+%   Author:
 %      Ildeberto de los Santos Ruiz
 %      idelossantos@ittg.edu.mx
+%      Certified MATLAB Associate
 %
 %   See also DOT.
 
